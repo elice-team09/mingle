123a7bc85c855fe81bcf9c6185229cfe53ff2437
youngjin son <ods04138@gmail.com>
2023-11-18 16:17:04 +0900

    redesigning sidebar

--- client/src/components/SideBarComponent/SideBarComponent.tsx
+++ client/src/components/SideBarComponent/SideBarComponent.tsx
@@ -1,94 +1,88 @@
-import React, { useState } from 'react'
-// import { Heart, Chart, Mypage, Fire, User, Home, Recent, Dropdown } from './styles';
+import React, { useState } from "react";
 import {
-  Nav,
-  Logo,
-  DivideLine,
-  ButtonsContainer,
-  ButtonWrapper,
-  Button,
-  DropdownButton,
-//   ButtonIcon,
-  Dropeddown,
-  DropeddownContents,
- 
+  StyledNav,
+  StyledLogo,
+  StyledDivideLine,
+  StyledButtonsContainer,
+  StyledButtonWrapper,
+  StyledButton,
+  StyledDropdownButton,
+  StyledDropeddown,
+  StyledDropeddownContents,
+} from "./styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faFire,
+  faDatabase,
+  faChartSimple,
+  faHeadphones,
+  faHeart,
+  faEllipsis,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
-//   Userlogo,
-} from './styles'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse, faFire, faDatabase, faChartSimple, faHeadphones, faHeart, faEllipsis, faUser } from '@fortawesome/free-solid-svg-icons'
+interface Item {
+  content: string;
+  icon: React.ReactNode;
+  dropdownItems?: string[];
+}
 
-
-
-
+const data = ["로그아웃", "회원정보 수정"];
 
 export default function SideBarComponent() {
-  const [isDropdown, setIsDropdown] = useState(false)
+  const [accordion, setAccordion] = useState<string | null>(null);
 
-  // const Dropdownbtn = () => {
-  //   setIsDropdown(!isDropdown)
-  // }
-
-  interface Item {
-    content: string
-    icon: React.ReactNode
-    dropdownItems?: string[]
-  }
-
-  const data = ['로그아웃', '회원정보 수정']
+  const toggleAccordion = (itemContent: string) => {
+    setAccordion((prev) => (prev === itemContent ? null : itemContent));
+  };
 
   const items: Item[] = [
     {
-      content: '내 정보',
-      icon: <FontAwesomeIcon icon={faUser} />, //logo는 아직  안넣어놈
-      stop: 'son',
+      content: "내 정보",
+      icon: <FontAwesomeIcon icon={faUser} />,
+      dropdownItems: data,
     },
-    { content: 'Feed', icon: <FontAwesomeIcon icon={faHouse} /> },
-    { content: '마이페이지', icon: <FontAwesomeIcon icon={faDatabase} /> },
-    { content: '추천 플레이리스트', icon: <FontAwesomeIcon icon={faFire} /> },
-    { content: '차트', icon: <FontAwesomeIcon icon={faChartSimple} /> },
-    { content: '최신음악', icon: <FontAwesomeIcon icon={faHeadphones} />},
-    { content: '좋아요한 음악', icon: <FontAwesomeIcon icon={faHeart} /> },
-  ]
+    { content: "Feed", icon: <FontAwesomeIcon icon={faHouse} /> },
+    { content: "마이페이지", icon: <FontAwesomeIcon icon={faDatabase} /> },
+    { content: "추천 플레이리스트", icon: <FontAwesomeIcon icon={faFire} /> },
+    { content: "차트", icon: <FontAwesomeIcon icon={faChartSimple} /> },
+    { content: "최신음악", icon: <FontAwesomeIcon icon={faHeadphones} /> },
+    { content: "좋아요한 음악", icon: <FontAwesomeIcon icon={faHeart} /> },
+  ];
 
   return (
-    <Nav>
-      <DivideLine>
-        <Logo>로고</Logo>
-      </DivideLine>
+    <StyledNav>
+      <StyledDivideLine>
+        <StyledLogo>
+          <img src="/img/Logo-Sidebar.png" alt="Logo" />
+        </StyledLogo>
+      </StyledDivideLine>
 
-      <ButtonsContainer>
-        
+      <StyledButtonsContainer>
         {items.map((item) => (
-          // <div key={idx} className=''>
-          <ButtonWrapper>
-              <Button
-                onClick={() => console.log('메뉴 아이템 : ', item.content)}>
-                <span>{item.icon}</span>
-                <span>{item.content}</span>
-                {item.stop && (
-                  <DropdownButton
-                    onClick={() => setIsDropdown((prev) => !prev)}>
-                    <FontAwesomeIcon icon={faEllipsis} />
-                  </DropdownButton>
-                )}
-                {isDropdown && item.stop && (
-                  <Dropeddown>
-                    {data.map((item, i) => (
-                      <DropeddownContents key={i}>
-                        <h4>{item}</h4>
-                      </DropeddownContents>
-                    ))}
-                  </Dropeddown>
-                )}
-              </Button>
-              </ButtonWrapper>
-          // </div>
+          <StyledButtonWrapper key={item.content}>
+            <StyledButton onClick={() => toggleAccordion(item.content)}>
+              <span>{item.icon}</span>
+              <span>{item.content}</span>
+              {item.dropdownItems && (
+                <StyledDropdownButton>
+                  <FontAwesomeIcon icon={faEllipsis} />
+                </StyledDropdownButton>
+              )}
+            </StyledButton>
+            {accordion === item.content && item.dropdownItems && (
+              <StyledDropeddown>
+                {item.dropdownItems.map((item, i) => (
+                  <StyledDropeddownContents key={i}>
+                    <h4>{item}</h4>
+                  </StyledDropeddownContents>
+                ))}
+              </StyledDropeddown>
+            )}
+          </StyledButtonWrapper>
         ))}
-        
-      </ButtonsContainer>
-    </Nav>
-  )
+      </StyledButtonsContainer>
+    </StyledNav>
+  );
 }
-
-
--- client/src/components/SideBarComponent/styles.ts
+++ client/src/components/SideBarComponent/styles.ts
@@ -1,36 +1,33 @@
+import tw, { styled, css } from "twin.macro";
 
-import tw,{ styled} from 'twin.macro';
-
-
-
-export const Nav = styled.nav`
+export const StyledNav = styled.nav`
   ${tw`
     fixed
     left-0
-      top-0
+    top-0
     bg-[#404040]
     box-border
     text-white
-    // top-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#404040] text-white p-4 flex flex-col
     h-full
     border-r-indigo-500
   `}
   width: 16rem;
-`
+`;
 
-export const Logo = styled.span`
+export const StyledLogo = styled.div`
   ${tw`
-    text-4xl font-semibold
+    text-4xl
+    font-semibold
   `}
-`
+`;
 
-export const DivideLine = styled.div`
+export const StyledDivideLine = styled.div`
   ${tw`
-    flex items-center justify-center h-20 border-b
+    flex items-center justify-center h-[201px] border-b
   `}
-`
+`;
 
-export const ButtonsContainer = styled.div`
+export const StyledButtonsContainer = styled.div`
   ${tw`
     flex 
     flex-col 
@@ -39,29 +36,37 @@ export const ButtonsContainer = styled.div`
     hover:border-slate-400 
     justify-between
   `}
-`
+`;
 
-export const ButtonWrapper = styled.div`
+export const StyledButtonWrapper = styled.div`
   ${tw`
     flex
     items-center space-x-3
   `}
   width: 16rem;
-`
+`;
 
-export const Button = styled.div`
+export const StyledButton = styled.div`
   ${tw`
     flex items-center space-x-3 p-2 
     hover:bg-zinc-900 
     hover:border-slate-400 
     pl-4
     py-4
+    relative
   `}
   width: 16rem;
-`
 
+  &:hover {
+    .additional-buttons {
+      opacity: 1;
+      visibility: visible;
+      transform: translateY(0);
+    }
+  }
+`;
 
-export const DropdownButton = styled.div`
+export const StyledDropdownButton = styled.div`
   ${tw`
     absolute
     left-56
@@ -70,42 +75,63 @@ export const DropdownButton = styled.div`
     ml-4
     w-5
   `}
-`
+`;
 
-export const Dropeddown = styled.div`
+export const StyledDropeddown = styled.div`
   ${tw`
     bg-gray-400 absolute top-28 left-60 flex flex-col items-start rounded-lg p-2 w-32 text-center
     ml-auto
   `}
-`
+`;
 
-
-
-export const DropeddownContents = styled.div`
+export const StyledDropeddownContents = styled.div`
   ${tw`
     flex w-full hover:bg-blue-300 cursor-pointer rounded-r-lg border-l-transparent
   `}
-`
+`;
 
-
-export const DropdownItem = styled.div`
+export const StyledDropdownItem = styled.div`
   ${tw`
     p-2 cursor-pointer hover:bg-gray-200
   `}
-`
+`;
 
-
-export const DropdownMenu = styled.div`
+export const StyledDropdownMenu = styled.div`
   absolute 
   flex
   flex-col
   top-20
   right-20
+`;
 
-`
-
-export const DropdownList = styled.ul`
+export const StyledDropdownList = styled.ul`
   flex
   flex-col 
   gap-4
-`
+`;
+
+export const StyledAdditionalButtons = styled.div`
+  ${tw`
+    absolute
+    bottom-0
+    left-0
+    w-full
+    p-2
+    opacity-0
+    transform translate-y-[-100%]
+    bg-zinc-900
+    border-t-2 border-slate-400
+    transition-all duration-300
+  `}
+`;
+
+export const StyledAdditionalButton = styled.div`
+  ${tw`
+    cursor-pointer
+    text-white
+    hover:text-gray-300
+  `}
+`;
+
+// ... (다른 스타일들 생략)
+
