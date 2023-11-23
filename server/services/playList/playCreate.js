6757f7824ae1b2c8551d926880b6b1a0282b21ca
limjongseung <101609328+limjongseung@users.noreply.github.com>
2023-11-23 21:01:09 +0900

    feature:추천플레이리스트 완성

--- server/services/playList/playCreate.js
+++ server/services/playList/playCreate.js
@@ -1,31 +1,40 @@
 const playListSchema = require("../../db/models/playListModel");
-const fs = require("fs"); 
+const createError = require("http-errors");
+const fs = require("fs");
 const path = require("path");
 
 /**
  * 플레이리스트 생성 함수
  * @param {object} jsonValue - 생성할 플레이리스트의 JSON 데이터
  * @param {string} userId - 생성한 플레이리스트의 사용자 ObjectId
- * @returns {Array} - [성공 여부(boolean), 메시지 객체(object)]
  */
 async function playListCreate(jsonValue, userId) {
-  try {
-    // 플레이리스트 데이터에 userId 추가
-    jsonValue.playListOwner = userId.toString();
-    if (jsonValue.playListImg) {
-      const decodedImage = Buffer.from(jsonValue.playListImg, 'base64');
-      const imagePath = path.join(__dirname, `../../upload/playListCover/${jsonValue.playListTitle}.png`);
-      fs.writeFileSync(imagePath, decodedImage);
-      jsonValue.userImage = imagePath;
+	try {
+		// 플레이리스트 데이터에 userId 추가
+		jsonValue.playListOwner = userId.toString();
+    if(!jsonValue.genre ){
+      throw createError(400,"genre가 없습니다.");
     }
-    // 플레이리스트 데이터를 생성하여 저장한다
-    const data = await playListSchema(jsonValue).save();
+		if (jsonValue.playListImg) {
+			const decodedImage = Buffer.from(jsonValue.playListImg, "base64");
+			const imagePath = path.join(
+				__dirname,
+				`../../upload/playListCover/${jsonValue.playListTitle}.png`
+			);
+			fs.writeFileSync(imagePath, decodedImage);
+			jsonValue.userImage = imagePath;
+		}
+		// 플레이리스트 데이터를 생성하여 저장한다
+		const data = await playListSchema(jsonValue).save();
 
-    return [true, { message: "플레이리스트가 생성되었습니다", playlistId: data._id.toString() }];
-  } catch (error) {
-    console.log(error);
-    return [false, { message: "플레이리스트 생성에 실패했습니다." }];
-  }
+		if (data) {
+			return { message: "플레이리스트가 생성되었습니다." };
+		} else {
+			throw createError(500, "플레이리스트 생성에 실패하였습니다.");
+		}
+	} catch (error) {
+		throw error;
+	}
 }
 
 module.exports = { playListCreate };
